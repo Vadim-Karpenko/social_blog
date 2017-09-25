ce4c1ce225108e7a36395ab7ef8713dd6f6a1cb2
rellay5 <myrka.rellay@gmail.com>
2017-09-25 16:47:54 +0300

    Profile model, and early follow system

--- account/models.py
+++ account/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from sorl.thumbnail import ImageField
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    date_of_birth = models.DateField('Your date of birth (YYYY-MM-DD)', blank=True, null=True)
+    photo = ImageField('', upload_to='users/%Y/%m/%d', default='users/default-photo.jpg')
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
+
+    def get_full_name(self):
+        return str(self.user.first_name) + ' ' + str(self.user.last_name)
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(User, related_name='rel_from_set')
+    user_to = models.ForeignKey(User, related_name='rel_to_set')
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{} folows {}'.format(self.user_from, self.user_to)
+
+# Add following field to User dynamically
+User.add_to_class('following', models.ManyToManyField('self', through=Contact, related_name='followers', symmetrical=False))
